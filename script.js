4d97d9b24cc50bb71dbf5bdd42c910279a54d655
Sabin Ghimire <sabinghimire223@gmail.com>
2025-02-03 19:49:46 +0545

    script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const clock = document.querySelector("#clock");
 // console.log(time.toLocaleTimeString());
 
 //syntax :
-// setInterval(function(){},1000 ); // 1000 is in milliseconds
+// setInterval(function(){},1000 ); // 1000 is in milliseconds which is 1 second
 
 //setInterval is a function that executes given function every given time interval.
 
@@ -11,4 +11,4 @@ setInterval(function () {
   const time = new Date();
 
   clock.innerHTML = `<span>${time.toLocaleTimeString()}</span>`;
-}, 1000);
+}, 10);
